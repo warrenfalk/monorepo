eb61ba5a7932133dbbc800347142571bc94f44c4
Warren Falk <warren@warrenfalk.com>
2020-02-25 21:36:20 -0500

    add json and js files to test

--- yarn1/fuse-app/src/index.ts
+++ yarn1/fuse-app/src/index.ts
@@ -1,12 +1,16 @@
+import {jsfile} from "node-lib/dist/jsfile"
 import { internalMessage } from "./internalModule";
 import { exportedFunc } from "node-lib"
 import { direct } from "node-lib/dist/direct"
 import * as uuid from "uuid"
+import * as json from "node-lib/dist/jsonfile.json"
 
 const message = `
 ${internalMessage()}<br>
 ${uuid()}<br>
 ${direct()}<br>
+${jsfile()}<br>
+${json}<br>
 ${exportedFunc()}<br>
 (refresh to update)
 `;
